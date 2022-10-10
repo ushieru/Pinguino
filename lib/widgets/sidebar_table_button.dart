4eaff0ee5af5c4ed45d99c55ff2c3a362592f8cd
ushieru <uzielcocolan@gmail.com>
2022-10-10 15:48:40 -0500

    Add SidebarTableButton

---
+++ lib/widgets/sidebar_table_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pinguino/routes/home/bloc/home_bloc.dart';
+
+class SidebarTableButton extends StatelessWidget {
+  const SidebarTableButton(this.tableName, {super.key});
+
+  final String tableName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 3),
+        child: TextButton(
+            onPressed: () =>
+                context.read<HomeBloc>().add(SelectTableEvent(tableName)),
+            child: Row(children: [
+              const Icon(Icons.table_chart_rounded),
+              const SizedBox(width: 10),
+              Text(tableName)
+            ])));
+  }
+}

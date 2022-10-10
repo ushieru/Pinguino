a0cd103451cd375263946a0ccbb3b0098c87bee0
ushieru <uzielcocolan@gmail.com>
2022-10-10 16:09:51 -0500

    Fix result rows remove in file widgets

--- lib/widgets/result_rows.dart
+++ lib/widgets/result_rows.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pinguino/routes/home/bloc/home_bloc.dart';
+import 'package:pinguino/widgets/result_row.dart';
 
 class ResultRows extends StatelessWidget {
   const ResultRows({super.key});
@@ -15,68 +16,8 @@ class ResultRows extends StatelessWidget {
           }
           return ListView(
               children: state.results
-                  .map<Widget>((row) => Row(
-                          children: row.values.map<Widget>((value) {
-                        if (value is Map) {
-                          return _ResultRowFK(value);
-                        }
-                        return _ResultRowSimple(value.toString());
-                      }).toList()))
+                  .map<Widget>((row) => ResultRow(row.values))
                   .toList());
         });
-  }
-}
-
-class _ResultRowSimple extends StatelessWidget {
-  const _ResultRowSimple(this.value);
-  final String value;
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-          height: 55,
-          decoration:
-              BoxDecoration(border: Border.all(color: Colors.indigo.shade200)),
-          child: TextField(
-              controller: TextEditingController(text: value),
-              enabled: false,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.zero)),
-              ))),
-    );
-  }
-}
-
-class _ResultRowFK extends StatelessWidget {
-  const _ResultRowFK(this.mapValue);
-  final Map mapValue;
-
-  @override
-  Widget build(BuildContext context) {
-    final value = mapValue['value'];
-    final query = mapValue['fk_query'];
-    return Expanded(
-      child: Container(
-          height: 55,
-          decoration:
-              BoxDecoration(border: Border.all(color: Colors.indigo.shade200)),
-          child: Row(children: [
-            Expanded(
-                child: TextField(
-                    controller: TextEditingController(text: value),
-                    enabled: false,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.zero)),
-                    ))),
-            ElevatedButton(
-                onPressed: () =>
-                    context.read<HomeBloc>().add(NewQueryEvent(query)),
-                style: ElevatedButton.styleFrom(fixedSize: const Size(55, 55)),
-                child: const Icon(Icons.link))
-          ])),
-    );
   }
 }

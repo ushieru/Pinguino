5970c9d5c34e064f711bf8963d62a3bb8691a5c2
ushieru <uzielcocolan@gmail.com>
2022-10-10 15:46:12 -0500

    Add main

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:pinguino/routes/home/home.dart';
+import 'package:pinguino/models/sqlite.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:flutter/material.dart';
+import 'package:pinguino/routes/connect.dart';
+
+void main() {
+  sqfliteFfiInit();
+  SQLite(
+      route: 'C:\\Users\\uziel\\Documents\\flutter\\pinguino\\red_social.db');
+  runApp(const Pinguino());
+}
+
+class Pinguino extends StatelessWidget {
+  const Pinguino({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.indigo),
+        title: 'Pingüino',
+        initialRoute: Connect.routeName,
+        routes: {
+          Connect.routeName: (context) => const Connect(),
+          Home.routeName: (context) => const Home(),
+        });
+  }
+}

eb4770f3ca4bd52229386a1260f530685fd9a17b
ushieru <uzielcocolan@gmail.com>
2022-10-13 17:53:06 -0500

    Delete unused imports

--- lib/widgets/results.dart
+++ lib/widgets/results.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:pinguino/routes/home/bloc/home_bloc.dart';
 import 'package:pinguino/widgets/result_rows.dart';
 import 'package:pinguino/widgets/results_headers.dart';
 

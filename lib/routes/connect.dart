7de1678f5d3a379f50f376477e91dfbd79269038
ushieru <uzielcocolan@gmail.com>
2022-10-13 16:37:10 -0500

    Add connect view

--- lib/routes/connect.dart
+++ lib/routes/connect.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:pinguino/models/sqlite.dart';
 import 'package:pinguino/routes/home/home.dart';
 
 class Connect extends StatelessWidget {
@@ -8,16 +10,40 @@ class Connect extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(children: [
-      Form(
-          child: Column(children: [
-        TextFormField(),
-        TextFormField(),
-        ElevatedButton(
-            onPressed: () => Navigator.pushNamedAndRemoveUntil(
-                context, Home.routeName, (route) => false),
-            child: const Text('Conectar'))
-      ]))
-    ]));
+        body: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      const Text('Pingüino',
+          style: TextStyle(
+              fontWeight: FontWeight.bold, color: Colors.indigo, fontSize: 50)),
+      const Text('SQLite client',
+          style: TextStyle(
+              fontWeight: FontWeight.bold, color: Colors.grey, fontSize: 20)),
+      const SizedBox(height: 20),
+      Image.asset('assets/icon.png'),
+      const SizedBox(height: 25),
+      SizedBox(
+          width: 400,
+          height: 40,
+          child: ElevatedButton(
+              onPressed: () async {
+                final path = await selectFile();
+                SQLite(
+                    route: path,
+                    onCreate: (_) async {
+                      Navigator.pushNamedAndRemoveUntil(
+                          context, Home.routeName, (route) => false);
+                    });
+              },
+              child: const Text('Select Database',
+                  style:
+                      TextStyle(fontWeight: FontWeight.w600, fontSize: 20)))),
+    ])));
   }
+}
+
+Future<String> selectFile() async {
+  FilePickerResult? result = await FilePicker.platform.pickFiles();
+  if (result == null) throw 'File not found';
+  return result.files.single.path!;
 }

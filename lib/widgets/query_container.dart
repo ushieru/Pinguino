fbe6b1b2e6df9245d3db7c6889b644c4f3536932
ushieru <uzielcocolan@gmail.com>
2022-11-16 23:09:06 -0600

    Add CodeField

--- lib/widgets/query_container.dart
+++ lib/widgets/query_container.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:code_text_field/code_text_field.dart';
+import 'package:highlight/languages/sql.dart';
+import 'package:flutter_highlight/themes/monokai-sublime.dart';
 import 'package:pinguino/routes/home/bloc/home_bloc.dart';
 
 class QueryContainer extends StatelessWidget {
@@ -8,26 +11,34 @@ class QueryContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<HomeBloc, HomeState>(
+    return BlocConsumer<HomeBloc, HomeState>(
         listenWhen: (previous, current) => current is NewQueryState,
         listener: (context, state) =>
             queryController.text = (state as NewQueryState).query,
-        child: Column(children: [
-          Container(
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.indigo.shade200, width: 2),
-                  borderRadius: const BorderRadius.all(Radius.circular(5))),
-              child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(controller: queryController, maxLines: 5))),
-          const SizedBox(height: 10),
-          Row(children: [
-            ElevatedButton(
-                onPressed: () => context
-                    .read<HomeBloc>()
-                    .add(NewQueryEvent(queryController.text)),
-                child: const Icon(Icons.play_arrow))
-          ])
-        ]));
+        buildWhen: (previous, current) => current is NewQueryState,
+        builder: (context, state) {
+          return Column(children: [
+            Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.indigo.shade200, width: 2),
+                    borderRadius: const BorderRadius.all(Radius.circular(5))),
+                child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CodeField(
+                        controller: CodeController(
+                            onChange: (query) => queryController.text = query,
+                            text: queryController.text,
+                            language: sql,
+                            theme: monokaiSublimeTheme)))),
+            const SizedBox(height: 10),
+            Row(children: [
+              ElevatedButton(
+                  onPressed: () => context
+                      .read<HomeBloc>()
+                      .add(NewQueryEvent(queryController.text)),
+                  child: const Icon(Icons.play_arrow))
+            ])
+          ]);
+        });
   }
 }
